2d9a8a97aaa24cea6f4a8ea2e61ec92ee66950e2
konectia <49333767+konectia@users.noreply.github.com>
2019-04-13 09:08:39 +0200

    Update azure-pipelines.yml for Azure Pipelines

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -28,9 +28,9 @@ module.exports = function (config) {
     // the default configuration
     junitReporter: {
       outputDir: '../coverage', // results will be saved as $outputDir/$browserName.xml
-      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: 'TESTS-RESULTS.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
+      useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
       properties: {} // key value pair of properties to add to the <properties> section of the report
